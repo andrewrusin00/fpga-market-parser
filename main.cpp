815c3c12dab0cf0b46817d5d6c8a52c9dd5db005
andrewrusin00 <andrew_rusin@outlook.com>
2025-06-09 18:48:15 -0400

    Parser now stores data(ID, side, etc) and puts it into a new file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include "main.hpp"
 
+std::vector<std::string> split(const std::string& s, char delim)
+{
+    std::vector<std::string> elems;
+    std::istringstream iss(s);
+    std::string item;
+    while(std::getline(iss, item, delim))
+    {
+        elems.push_back(item);
+    }
+
+    return elems;
+}
+
+// Put these into classes later
+double totalBuyQty = 0.0;
+double totalSellQty = 0.0;
+
+double bestBid = 0.0;
+double bestAsk = std::numeric_limits<double>::max();
+
 
 int main()
 {
@@ -13,6 +33,8 @@ int main()
         return 1;
     }
 
+    std::vector<TradeObjects_t> trades;
+
     while (std::getline(file, line))
     {
         std::cout << "Read line: " <<  line << '\n';
@@ -22,18 +44,50 @@ int main()
             std::cerr << "Bad line: " << line << "\n";
             continue;
         }
+        TradeObjects_t trade;
+        trade.timestamp = parts[0];
+        trade.side = parts[1];
+        trade.quantity = std::stod(parts[2]);
+        trade.price = std::stod(parts[3]);
 
-        catagories.timestamp = parts[0];
-        catagories.side = parts[1];
-        catagories.quantity = std::stod(parts[2]);
-        catagories.price = std::stod(parts[3]);
+        static int currentID = 1;
+        trade.id = currentID++;
 
-    std::cout << "Timestamp: " << catagories.timestamp << "\n"
-              << "Side:      " << catagories.side      << "\n"
-              << "Quantity:  " << catagories.quantity  << "\n"
-              << "Price:     " << catagories.price     << "\n";
+        if (trade.side == "BUY")
+        {
+            totalBuyQty += trade.quantity;
+            if (trade.price > bestBid)
+            {
+                bestBid = trade.price;
+            }
+        }
+        else if (trade.side == "SELL")
+        {
+            totalSellQty += trade.quantity;
+            if(trade.price < bestAsk)
+            {
+                bestAsk = trade.price;
+            }
+        }
+
+        trades.push_back(trade);
+    
+        std::ofstream out("parsed_output.txt");
+        for (const auto& trade : trades)
+        {
+            out << "Trade ID: " << trade.id << "\n"
+                << "Timestamp: " << trade.timestamp << "\n"
+                << "Side:      " << trade.side      << "\n"
+                << "Quantity:  " << trade.quantity  << "\n"
+                << "Price:     " << trade.price     << "\n";
+        }
 
     }
+
+    std::cout << "Total BUY Quantity: " << totalBuyQty << "\n";
+    std::cout << "Total SELL Quantity: " << totalSellQty << "\n";
+    std::cout << "Best BID: " << bestBid << "\n";
+    std::cout << "Best ASK: " << bestAsk << "\n";
 
     return 0;
 }
--- main.hpp
+++ main.hpp
@@ -6,26 +6,18 @@
 #include <cstdint>
 #include <vector>
 #include <sstream>
-
-std::vector<std::string> split(const std::string& s, char delim)
-{
-    std::vector<std::string> elems;
-    std::istringstream iss(s);
-    std::string item;
-    while(std::getline(iss, item, delim))
-    {
-        elems.push_back(item);
-    }
-
-    return elems;
-}
+#include <limits>
 
 struct TradeObjects_t{
+    int id;
     std::string timestamp;
     std::string side;
     double quantity;
     double price;
 };
 
-TradeObjects_t catagories = {"None", "None", 0.0d, 0.0d};
+std::vector<std::string> split(const std::string& s, char delim);
+
+
+
     
